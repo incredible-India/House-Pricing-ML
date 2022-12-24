68685573e3be3c4a53c38116be33454c9e3b0bb6
himanshu sharma <iamonlyhimanshusharma@gmail.com>
2022-12-24 18:03:26 +0530

    model working good added js file for ajax request

--- HousePricing/prediction/views.py
+++ HousePricing/prediction/views.py
@@ -3,9 +3,14 @@ import json
 from django.contrib import messages
 from django.views import View
 import pickle
+import numpy as np
 # Create your views here.
 
-
+__MLmodel = None
+locations = None
+sqft =None
+bath = None
+bhk= None
 #location from bengaluru test datsets
 with open('./prediction/citilocation/columns.json') as cl:
     locations = json.loads(cl.read())
@@ -20,25 +25,44 @@ with open('./prediction/citilocation/bath.json') as ps:
 with open('./prediction/citilocation/bhk.json') as bs:
     bhk = json.loads(bs.read())
 
+with open('./prediction/ML_Model_and_datasets/Mymodel.pickle','rb') as LM:
+    __MLmodel = pickle.load(LM)
+
+
+
 
 
 #using our trained model price prediction
 #here we use our traine model is in ./House Pricing BNG\HousePricing\prediction\ML Model and datasets
 #for more info u can check that directory and open DHouse Pricing BNG\HousePricing\prediction\ML_Model_and_datasets\.ipynb_checkpoints\Housing Pricing-checkpoint.ipynb with jupyter notebook
 def CalculateEstimatePrice(location,sqft,bath,bhk):
+
+    global locations
+    global __MLmodel
+
     
-    with open('./prediction/ML_Model_and_datasets/Mymodel.pickle','rb') as Linearmodel:
+    
+    try:
+        #checking the loacation as columns in my datsets
+        loc_index = locations['data_columns'][3:].index(location)
+    except:
+        loc_index = -1
+  
+    xt = np.zeros(len(locations['data_columns']))
+    xt[0] = sqft
+    xt[1] = bath
+    xt[2] = bhk
+    if loc_index >= 0:
+        xt[loc_index] = 1
 
-        model = pickle.load(Linearmodel)
-
-        pricrHome = model.predict([[location,sqft,bath,bhk]])
-
-        return pricrHome
+    return __MLmodel.predict([xt])[0]
 
 
 class index(View):
 
     def get(self, request):
+
+        global location, sqft,bath,bhk
 
         return render(request, 'prediction/prediction.html',{
             'locations' : locations['data_columns'][3:],
@@ -58,9 +82,11 @@ class index(View):
         #this is function which calculate the home price using our trained model ,and our trained model take argument as loaction,square fott,bath ,bhk (in order)
         estimatePrice = CalculateEstimatePrice(userlocation, usersqft,userbath, userbhk)
 
+        finalPrice = round(estimatePrice,2)
 
 
 
 
 
-        return HttpResponse(f'{estimatePrice},{usersqft},{userbath} ,{userbhk}')
+
+        return HttpResponse(f'{finalPrice},{usersqft},{userbath} ,{userbhk}')
